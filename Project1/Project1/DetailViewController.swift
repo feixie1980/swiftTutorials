bfea8fef5cd185d4e60c7839ffeb3bc0f5e8131a
Fei Xie <>
2020-01-21 18:13:05 -0800

    save

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -9,43 +9,55 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  var selectedImage: String?
+  var detailViewTitle: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet var imageView: UIImageView!
+    if let imageToLoad = selectedImage {
+      imageView.image  = UIImage(named: imageToLoad)
+    }
+    // Do any additional setup after loading the view.
     
-    var selectedImage: String?
-    var detailViewTitle: String?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let imageToLoad = selectedImage {
-            imageView.image  = UIImage(named: imageToLoad)
-        }
-        // Do any additional setup after loading the view.
-        
-        title = detailViewTitle
-        navigationItem.largeTitleDisplayMode = .never
+    title = detailViewTitle
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    navigationItem.largeTitleDisplayMode = .never
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated);
+    navigationController?.hidesBarsOnTap = true;
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated);
+    navigationController?.hidesBarsOnTap = false;
+  }
+  
+  @objc func shareTapped() {
+    guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+      print("No image found")
+      return
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated);
-        navigationController?.hidesBarsOnTap = true;
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated);
-        navigationController?.hidesBarsOnTap = false;
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+    vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+    present(vc, animated: true)
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
